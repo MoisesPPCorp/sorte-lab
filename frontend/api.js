31c6cff2ba8d28533f3c74cf145f976670d7e892
Moises Paixao <moisesppcorp@gmail.com>
2026-03-22 20:49:08 -0300

    fix frontend api railway correto

--- frontend/api.js
+++ frontend/api.js
@@ -1,12 +1,14 @@
+const API = "https://sorte-lab-production.up.railway.app";
+
 function getToken() {
     return localStorage.getItem("token");
 }
 
-async function apiFetch(url, options = {}) {
+async function apiFetch(endpoint, options = {}) {
 
     const token = getToken();
 
-    const response = await fetch(url, {
+    const response = await fetch(API + endpoint, {
         ...options,
         headers: {
             "Content-Type": "application/json",
